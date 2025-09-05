fb5476a49c1e1c2aa2f0604a83755e35c592802b
jessicaagarwal <jessica.agarwal.s64@kalvium.community>
2025-09-05 16:53:49 +0530

    part01 commit

---
+++ src/com/school/Main.java
@@ -0,0 +1,10 @@
+package com.school;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] var0) {
+        System.out.println("Welcome to the Attendance Management System!");
+    }
+}
